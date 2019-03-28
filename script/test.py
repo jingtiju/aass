9d4a6b41e9a25fa8878fad126445f7ebe7a35a0d
admin <1251659707@qq.com>
2019-03-28 16:31:59 +0800

    aa

---
+++ script/test.py
@@ -0,0 +1,34 @@
+import allure, pytest
+
+class Test_1:
+    @allure.step(title="66666")
+    def test_1(self):
+        print("*" * 50)
+        allure.attach("111", "111111", allure.attach_type.TEXT)
+        with open("./cc.jpg", "rb") as f:
+            allure.attach("aa", f.read(), allure.attach_type.JPG)
+        assert True
+    @pytest.allure.serverity(pytest.allure.serverity_level.BLOCKER)
+    def test_2(self):
+        print("#" * 50)
+        allure.attach("2222", "2222", allure.attach_type.TEXT)
+
+    @pytest.allure.serverity(pytest.allure.serverity_level.CRITICAL)
+    def test_3(self):
+        print("#" * 50)
+        allure.attach("2222", "2222", allure.attach_type.TEXT)
+
+    @pytest.allure.serverity(pytest.allure.serverity_level.NORMAL)
+    def test_4(self):
+        print("#" * 50)
+        allure.attach("2222", "2222", allure.attach_type.TEXT)
+
+    @pytest.allure.serverity(pytest.allure.serverity_level.MINOR)
+    def test_5(self):
+        print("#" * 50)
+        allure.attach("2222", "2222", allure.attach_type.TEXT)
+
+    @pytest.allure.serverity(pytest.allure.serverity_level.TRIVIAL)
+    def test_6(self):
+        print("#" * 50)
+        allure.attach("2222", "2222", allure.attach_type.TEXT)
